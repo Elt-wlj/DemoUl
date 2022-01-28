9de80c39ac74bf2734218b5237462888220af1ad
Elt-wlj <2208078600@qq.com>
2022-01-28 17:42:02 +0800

    完整测试框架

---
+++ page_obj/LoginPage.py
@@ -0,0 +1,4 @@
+# 封装登录页面对象类，执行登录测试流程
+import os,sys
+from selenium import webdriver
+from time import sleep
---
+++ run_demoui_test.py
@@ -0,0 +1,2 @@
+# 这是主函数运行模块
+https://github.com/yingoja/DemoUI/blob/master/run_demoui_test.py 
